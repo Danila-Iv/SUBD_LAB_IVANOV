fb57a64b208c1dbc4e2698caf655883451e92555
Danila-Iv <71919980+Danila-Iv@users.noreply.github.com>
2021-12-28 22:55:21 +0300

    Update student.dto.ts

--- test_subd/backend/src/student/dto/student.dto.ts
+++ test_subd/backend/src/student/dto/student.dto.ts
@@ -1,8 +1,8 @@
 export class StudentDto {
     readonly firstname: string;
     readonly secondname: string;
-    readonly birthday: Date;
+    readonly birthday: string;
     readonly university: string;
     readonly faculty: string;
-    readonly applyDate: Date;
+    readonly applyDate: string;
   }
